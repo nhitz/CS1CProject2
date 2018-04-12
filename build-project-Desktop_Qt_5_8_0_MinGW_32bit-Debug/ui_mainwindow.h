f6d2aeb1e304d8422b1b0ca2af3d63b4a92520a4
naomidonato <36245167+naomidonato@users.noreply.github.com>
2018-04-11 17:24:40 -0700

    Add files via upload

--- build-project-Desktop_Qt_5_8_0_MinGW_32bit-Debug/ui_mainwindow.h
+++ build-project-Desktop_Qt_5_8_0_MinGW_32bit-Debug/ui_mainwindow.h
@@ -13,12 +13,13 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QCommandLinkButton>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
-#include <QtWidgets/QPushButton>
-#include <QtWidgets/QScrollArea>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
@@ -30,11 +31,14 @@ class Ui_MainWindow
 public:
     QWidget *centralWidget;
     QLabel *label;
-    QPushButton *pushButton_2;
-    QPushButton *pushButton_3;
-    QScrollArea *scrollArea;
-    QWidget *scrollAreaWidgetContents;
-    QPushButton *pushButton;
+    QLabel *label_2;
+    QFrame *frame;
+    QLabel *label_3;
+    QWidget *layoutWidget;
+    QHBoxLayout *horizontalLayout;
+    QCommandLinkButton *commandLinkButton_2;
+    QCommandLinkButton *commandLinkButton_3;
+    QCommandLinkButton *AdminLogin;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
@@ -43,43 +47,65 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(1093, 669);
+        MainWindow->resize(1308, 780);
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
         label = new QLabel(centralWidget);
         label->setObjectName(QStringLiteral("label"));
-        label->setGeometry(QRect(30, 20, 201, 51));
+        label->setGeometry(QRect(30, 20, 231, 51));
         QFont font;
         font.setFamily(QStringLiteral("MS Sans Serif"));
-        font.setPointSize(19);
+        font.setPointSize(22);
         font.setBold(true);
         font.setWeight(75);
         label->setFont(font);
-        pushButton_2 = new QPushButton(centralWidget);
-        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
-        pushButton_2->setGeometry(QRect(850, 50, 121, 23));
-        pushButton_3 = new QPushButton(centralWidget);
-        pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
-        pushButton_3->setGeometry(QRect(480, 50, 81, 23));
-        scrollArea = new QScrollArea(centralWidget);
-        scrollArea->setObjectName(QStringLiteral("scrollArea"));
-        scrollArea->setGeometry(QRect(0, 0, 1091, 631));
-        scrollArea->setWidgetResizable(true);
-        scrollAreaWidgetContents = new QWidget();
-        scrollAreaWidgetContents->setObjectName(QStringLiteral("scrollAreaWidgetContents"));
-        scrollAreaWidgetContents->setGeometry(QRect(0, 0, 1089, 629));
-        pushButton = new QPushButton(scrollAreaWidgetContents);
-        pushButton->setObjectName(QStringLiteral("pushButton"));
-        pushButton->setGeometry(QRect(670, 50, 121, 23));
-        scrollArea->setWidget(scrollAreaWidgetContents);
+        label_2 = new QLabel(centralWidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setGeometry(QRect(30, 70, 211, 16));
+        QFont font1;
+        font1.setPointSize(11);
+        label_2->setFont(font1);
+        frame = new QFrame(centralWidget);
+        frame->setObjectName(QStringLiteral("frame"));
+        frame->setGeometry(QRect(40, 160, 1061, 271));
+        frame->setFrameShape(QFrame::StyledPanel);
+        frame->setFrameShadow(QFrame::Raised);
+        label_3 = new QLabel(frame);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setGeometry(QRect(20, 10, 81, 16));
+        QFont font2;
+        font2.setFamily(QStringLiteral("Yu Gothic UI Semibold"));
+        font2.setPointSize(11);
+        font2.setBold(true);
+        font2.setWeight(75);
+        label_3->setFont(font2);
+        layoutWidget = new QWidget(centralWidget);
+        layoutWidget->setObjectName(QStringLiteral("layoutWidget"));
+        layoutWidget->setGeometry(QRect(429, 40, 561, 41));
+        horizontalLayout = new QHBoxLayout(layoutWidget);
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        commandLinkButton_2 = new QCommandLinkButton(layoutWidget);
+        commandLinkButton_2->setObjectName(QStringLiteral("commandLinkButton_2"));
+
+        horizontalLayout->addWidget(commandLinkButton_2);
+
+        commandLinkButton_3 = new QCommandLinkButton(layoutWidget);
+        commandLinkButton_3->setObjectName(QStringLiteral("commandLinkButton_3"));
+
+        horizontalLayout->addWidget(commandLinkButton_3);
+
+        AdminLogin = new QCommandLinkButton(layoutWidget);
+        AdminLogin->setObjectName(QStringLiteral("AdminLogin"));
+
+        horizontalLayout->addWidget(AdminLogin);
+
         MainWindow->setCentralWidget(centralWidget);
-        scrollArea->raise();
-        label->raise();
-        pushButton_2->raise();
-        pushButton_3->raise();
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 1093, 21));
+        menuBar->setGeometry(QRect(0, 0, 1308, 21));
         MainWindow->setMenuBar(menuBar);
         mainToolBar = new QToolBar(MainWindow);
         mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
@@ -97,9 +123,11 @@ public:
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
         label->setText(QApplication::translate("MainWindow", "ICyberSecurity", Q_NULLPTR));
-        pushButton_2->setText(QApplication::translate("MainWindow", "Administrator Login", Q_NULLPTR));
-        pushButton_3->setText(QApplication::translate("MainWindow", "Help", Q_NULLPTR));
-        pushButton->setText(QApplication::translate("MainWindow", "Customer Login", Q_NULLPTR));
+        label_2->setText(QApplication::translate("MainWindow", "Your Protection Is Our Success", Q_NULLPTR));
+        label_3->setText(QApplication::translate("MainWindow", "About Us", Q_NULLPTR));
+        commandLinkButton_2->setText(QApplication::translate("MainWindow", "Help", Q_NULLPTR));
+        commandLinkButton_3->setText(QApplication::translate("MainWindow", "Customer Login", Q_NULLPTR));
+        AdminLogin->setText(QApplication::translate("MainWindow", "Administrator Login", Q_NULLPTR));
     } // retranslateUi
 
 };
