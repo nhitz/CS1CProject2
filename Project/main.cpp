d21790089ad3d5aa6540844fc29549431b790094
Rothcolt <mw.rothcolt@gmail.com>
2018-05-06 18:32:01 -0700

    Added Doxygen comments in block format.

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+/********************************************//**
+ *  The overarching primary functionality.
+ ***********************************************/
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
