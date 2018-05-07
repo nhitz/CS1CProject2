2f98ec381655475287e6ef8ec3f0424a619afedf
Rothcolt <mw.rothcolt@gmail.com>
2018-05-06 18:12:55 -0700

    Added Doxygen comments in block format.

--- Project/customerwindow.h
+++ Project/customerwindow.h
@@ -3,10 +3,16 @@
 
 #include <QDialog>
 
+/********************************************//**
+ *  Namespace for the User Interface.
+ ***********************************************/
 namespace Ui {
   class CustomerWindow;
 }
 
+/********************************************//**
+ *  Object class for the Customer Window.
+ ***********************************************/
 class CustomerWindow : public QDialog
 {
   Q_OBJECT
